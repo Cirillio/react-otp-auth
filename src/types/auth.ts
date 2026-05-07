d7eec4ebc5efb9c5bc4eb73e3cd424bdf5231837
Cirillio <ineed2healing@gmail.com>
2026-05-07 18:53:20 +0700

    base setup

---
+++ src/types/auth.ts
@@ -0,0 +1,5 @@
+import type { components } from "./api";
+
+export type User = components["schemas"]["User"];
+export type SignInRequest = components["schemas"]["SignInDto"];
+export type OtpResponse = components["schemas"]["OtpResponse"];
